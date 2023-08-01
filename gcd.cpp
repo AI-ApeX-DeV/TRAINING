3902309efa61ec144a30aaf8770304f7125f2511
SYED SHARUKHALI FAROOKALI NAZIYABEGUM <shahsai11111@gmail.com>
2023-08-01 13:19:27 +0530

    done

---
+++ gcd.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "head.h"
+using namespace std;
+using namespace SHAH;
+int main()
+{
+    cout<< GCD_recursive(205,328);
+}
---
+++ lcm.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "head.h"
+using namespace std;
+using namespace SHAH;
+int main()
+{
+    cout<<LCM(5,7);
+}
