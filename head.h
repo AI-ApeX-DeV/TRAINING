7fba8508c9bf7d4572b24501ce1db049b31f4499
SYED SHARUKHALI FAROOKALI NAZIYABEGUM <shahsai11111@gmail.com>
2023-08-01 13:01:55 +0530

    done

---
+++ head.h
@@ -0,0 +1,7 @@
+namespace SHAH
+{
+    int shark(int a ,int b)
+    {
+        return a+b;
+    }
+}
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "head.h"
+using namespace std;
+using namespace SHAH;
+int main()
+{
+    cout<<shark(2,3)<<endl;
+    return 0;
+}
