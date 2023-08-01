779fdf56af01f6b9e2ecedce4ac4f86da289fb86
SYED SHARUKHALI FAROOKALI NAZIYABEGUM <shahsai11111@gmail.com>
2023-08-01 13:16:33 +0530

    done

--- head.h
+++ head.h
@@ -4,4 +4,31 @@ namespace SHAH
     {
         return a+b;
     }
+    int GCD_recursive(int a,int b)
+    {
+        if(a%b==0)
+        {
+            return b;
+        }
+        else
+        {
+            return GCD_recursive(b,a%b);
+        }
+    }
+    int GCD_iterative(int a,int b)
+    {
+        int m;
+        while(a%b!=0)
+        {
+            m=b;
+            b=a%b;
+            a=m;
+        }
+        return b;
+    }
+    int LCM(int a ,int b)
+    {
+        return (a*b)/GCD_iterative(a,b);
+    }
+    
 }
