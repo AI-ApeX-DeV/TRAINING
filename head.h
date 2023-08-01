95b180e8dec68d865a6c6821e6fde7282d755e24
SYED SHARUKHALI FAROOKALI NAZIYABEGUM <shahsai11111@gmail.com>
2023-08-01 13:57:21 +0530

    done

--- head.h
+++ head.h
@@ -1,3 +1,5 @@
+#include<cmath>
+using namespace std;
 namespace SHAH
 {
     int shark(int a ,int b)
@@ -30,5 +32,21 @@ namespace SHAH
     {
         return (a*b)/GCD_iterative(a,b);
     }
-    
+
+    double bisection(double a, double b, int root, double num) {
+        double mid;
+        double epsilon = 0.000001;
+        while (abs(b - a) >= epsilon) { // Use std::abs to get the absolute value
+            mid = (a + b) / 2;
+            if (pow(a, root) - num > 0 && pow(mid, root) - num > 0 ||
+                pow(a, root) - num < 0 && pow(mid, root) - num < 0) {
+                a = mid;
+            } else {
+                b = mid;
+            }
+        }
+        return b;
+    }
+
+
 }
---
+++ root.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "head.h"
+using namespace std;
+using namespace SHAH;
+int main()
+{
+    cout<<bisection(0,2,9,2)<<endl;
+    cout<<pow(2, 1.0 / 9.0);
+}
