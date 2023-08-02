b33f38ebe7dc3adb43a78ef972b486fc8e7db138
SYED SHARUKHALI FAROOKALI NAZIYABEGUM <shahsai11111@gmail.com>
2023-08-03 00:27:16 +0530

    done

---
+++ classprivateuse.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+class Rectangle {
+private:
+    int width;
+    int height;
+
+    int calculateArea() {
+        return width * height;
+    }
+
+public:
+    Rectangle(int w, int h) {
+        width = w;
+        height = h;
+    }
+
+    int getWidth() {
+        return width;
+    }
+
+    int getHeight() {
+        return height;
+    }
+
+    void setDimensions(int newWidth, int newHeight) {
+        width = newWidth;
+        height = newHeight;
+    }
+
+    int getArea() {
+        // Call the private helper function to calculate the area
+        return calculateArea();
+    }
+
+    int calculatePerimeter() {
+        return 2 * (width + height);
+    }
+};
+
+int main() {
+    Rectangle rect(5, 3);
+
+    std::cout << "Width: " << rect.getWidth() << ", Height: " << rect.getHeight() << std::endl;
+
+    // Error: 'int Rectangle::calculateArea()' is private within this context
+    // std::cout << "Area: " << rect.calculateArea() << std::endl;
+
+    // Instead, use the public member function that calls the private helper function.
+    std::cout << "Area: " << rect.getArea() << std::endl;
+    std::cout << "Perimeter: " << rect.calculatePerimeter() << std::endl;
+
+    return 0;
+}
