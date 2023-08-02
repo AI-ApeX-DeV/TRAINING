ea472461e0b9ff30a1b7e0cdd4f549fca9841948
SYED SHARUKHALI FAROOKALI NAZIYABEGUM <shahsai11111@gmail.com>
2023-08-02 17:31:53 +0530

    done

---
+++ constructors.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+struct cons
+{
+    int a,b,c;
+    cons()
+    {
+        a=b=c=3;
+        cout<<"the value of a,b,c in intitial constructor are : "<<a<<" "<<b<<" "<<c<<endl;
+    }
+    cons(int z)
+    {
+        a=b=c=z;
+        cout<<"the value of a,b,c in second constructor are : "<<a<<" "<<b<<" "<<c<<endl;
+    }
+};
+int main()
+{
+    cons a(99);
+    cons b;
+    return 0;
+    
+}
