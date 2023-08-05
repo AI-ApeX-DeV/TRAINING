5ed709eeef1408040038d54ace3e06d573ae1ec9
SYED SHARUKHALI FAROOKALI NAZIYABEGUM <shahsai11111@gmail.com>
2023-08-05 11:07:21 +0530

    done

--- vectors.cpp
+++ vectors.cpp
@@ -17,4 +17,18 @@ int main()
     cout<<v.size()<<endl;
     cout<<v.capacity()<<endl;
     cout<<v[4]<<endl;
+
+    vector<vector<int> > v2(5,vector<int>(5,1));
+    cout<<v2.size()<<endl;
+    cout<<v2[0].size()<<endl;
+
+    for(size_t i=0;i<v2.size();i++)
+    {
+        for(size_t j=0;j<v2[i].size();j++)
+        {
+            cout<<v2[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
 }
